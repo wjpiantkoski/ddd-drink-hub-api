22dbbf4ec4663271b80ed05ecde3198606930745
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-17 17:30:59 -0300

    feat: add UserRepository's findOneByEmail method

--- src/infra/database/sequelize/user/user.repository.spec.ts
+++ src/infra/database/sequelize/user/user.repository.spec.ts
@@ -2,6 +2,7 @@ import { Sequelize } from "sequelize-typescript"
 import { UserModel } from "./user.model"
 import UserRepository from "./user.repository"
 import User from "../../../../modules/user/domain/user.entity"
+import {v4 as uuidv4} from 'uuid'
 
 describe('UserRepository', () => {
   let sequelize: Sequelize
@@ -33,5 +34,22 @@ describe('UserRepository', () => {
 
     expect(result).toBeDefined()
     expect(result.id).toEqual(user.id)
+  })
+
+  it('should get a user by email', async () => {
+    const user = {
+      id: uuidv4(),
+      name: 'Name',
+      email: 'email@email.com',
+      password: 'any-pass'
+    }
+
+    await UserModel.create(user)
+
+    const repository = new UserRepository()
+    const result = await repository.findByEmail(user.email)
+
+    expect(result).toBeDefined()
+    expect(result.email).toEqual(user.email)
   })
 })
--- src/infra/database/sequelize/user/user.repository.ts
+++ src/infra/database/sequelize/user/user.repository.ts
@@ -2,23 +2,29 @@ import User from "../../../../modules/user/domain/user.entity";
 import IUserRepository from "../../../../modules/user/repository/user.repository.interface";
 import { UserModel } from "./user.model";
 
-
 export default class UserRepository implements IUserRepository {
-  async create(data: User): Promise<void> {
-    try {
-      await UserModel.create({
-        id: data.id,
-        name: data.name,
-        email: data.email,
-        password: data.password
-      })
-    } catch (err) {
-      console.error(err)
+  async findByEmail(email: string): Promise<User> {
+    const user = await UserModel.findOne({ where: { email } })
+
+    if (!user) {
+      return null
     }
+
+    return new User(user.name, user.email, user.password, user.id)
   }
-  findOne(query: any): Promise<any> {
+  
+  findById(id: string): Promise<User> {
     throw new Error("Method not implemented.");
   }
+  async create(data: User): Promise<void> {
+    await UserModel.create({
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      password: data.password
+    })
+  }
+
   updateOne(id: string, data: any): Promise<void> {
     throw new Error("Method not implemented.");
   }
