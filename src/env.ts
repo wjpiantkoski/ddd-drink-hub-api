3302507950f76753a9ae829cbca99a04296e0fed
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-17 16:42:07 -0300

    revert: add jwt env values

--- src/env.ts
+++ src/env.ts
@@ -3,3 +3,5 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const PASSWORD_HASH_SALT_ROUNDS = process.env.PASSWORD_HASH_SALT_ROUNDS
+export const JWT_SECRET = process.env.JWT_SECRET
+export const JWT_EXPIRATION_TIME = process.env.JWT_EXPIRATION_TIME
