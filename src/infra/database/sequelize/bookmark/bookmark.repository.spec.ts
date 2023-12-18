0ab8f54d26d7fba65be631e11af9355355a7c026
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-18 06:54:53 -0300

    feat: add all BookmarkRepository methods

--- src/infra/database/sequelize/bookmark/bookmark.repository.spec.ts
+++ src/infra/database/sequelize/bookmark/bookmark.repository.spec.ts
@@ -62,4 +62,60 @@ describe('BookmarkRepository', () => {
 
     expect(result).toBeDefined()
   })
+
+  it('should get all bookmarks from an user', async () => {
+    const categoryRepository = new CategoryRepository()
+    const category = new Category('Category Name')
+
+    await categoryRepository.create(category)
+
+    const beverageRepository = new BeverageRepository()
+    const beverage = new Beverage({
+      category,
+      name: 'Beverage',
+      userId: uuidv4(),
+      description: 'Description'
+    })
+
+    await beverageRepository.create(beverage)
+
+    const bookmark = new Bookmark(beverage.userId, beverage)
+    const repository = new BookmarkRepository()
+
+    await repository.create(bookmark)
+
+    const result = await repository.findAllByUserId(bookmark.userId)
+
+    expect(result.length).toBeGreaterThan(0)
+    expect(result[0].userId).toEqual(bookmark.userId)
+  })
+
+  it('should remove bookmark', async () => {
+    const categoryRepository = new CategoryRepository()
+    const category = new Category('Category Name')
+
+    await categoryRepository.create(category)
+
+    const beverageRepository = new BeverageRepository()
+    const beverage = new Beverage({
+      category,
+      name: 'Beverage',
+      userId: uuidv4(),
+      description: 'Description'
+    })
+
+    await beverageRepository.create(beverage)
+
+    const bookmark = new Bookmark(beverage.userId, beverage)
+    const repository = new BookmarkRepository()
+
+    await repository.create(bookmark)
+    await repository.deleteById(bookmark.id)
+
+    const result = await BookmarkModel.findOne({
+      where: { id: bookmark.id }
+    })
+
+    expect(result).toBeNull()
+  })
 })
--- src/infra/database/sequelize/bookmark/bookmark.repository.ts
+++ src/infra/database/sequelize/bookmark/bookmark.repository.ts
@@ -1,5 +1,9 @@
+import Beverage from "../../../../modules/beverage/domain/beverage/beverage.entity";
 import Bookmark from "../../../../modules/beverage/domain/bookmark/bookmark.entity";
+import Category from "../../../../modules/beverage/domain/category/category.entity";
 import IBookmarkRepository from "../../../../modules/beverage/repository/bookmark.repository.interface";
+import BeverageModel from "../beverage/beverage.model";
+import CategoryModel from "../category/category.model";
 import BookmarkModel from "./bookmark.model";
 
 export default class BookmarkRepository implements IBookmarkRepository {
@@ -11,11 +15,34 @@ export default class BookmarkRepository implements IBookmarkRepository {
     })
   }
 
-  findAllByUserId(userId: string): Promise<Bookmark[]> {
-    throw new Error("Method not implemented.");
+  async findAllByUserId(userId: string): Promise<Bookmark[]> {
+    const bookmarks = await BookmarkModel.findAll({
+      where: { userId },
+      include: [
+        {
+          model: BeverageModel,
+          include: [CategoryModel]
+        }
+      ]
+    })
+
+    return bookmarks.map(bookmark => {
+      const category = new Category(bookmark.beverage.category.name, bookmark.beverage.category.id)
+      
+      const beverage = new Beverage({
+        category,
+        id: bookmark.beverage.id,
+        name: bookmark.beverage.name,
+        userId: bookmark.beverage.userId,
+        description: bookmark.beverage.description
+      })
+
+      return new Bookmark(bookmark.userId, beverage, bookmark.id)
+    })
   }
-  deleteById(id: string): Promise<void> {
-    throw new Error("Method not implemented.");
+
+  async deleteById(id: string): Promise<void> {
+    await BookmarkModel.destroy({ where: { id } })
   }
   
 }
