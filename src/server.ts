7e8829f45138557b57a3c84fc0ff08b21a1c4b08
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-18 10:25:30 -0300

    feat: add populateCategoriesService call at server initialization

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import App from "./infra/http/express"
 import {SERVER_PORT} from './env'
 import Database from "./infra/@shared/database/database"
+import PopulateCategoriesServiceFactory from "./modules/beverage/services/populate-categories/populate-categories.service.factory"
 
 class Server {
   private app
@@ -14,6 +15,9 @@ class Server {
 
     await database.connect()
     console.log('Database is connected')
+
+    const populateCategories = PopulateCategoriesServiceFactory.create()
+    await populateCategories.run()
 
     const server = this.app.listen(SERVER_PORT)
     console.log(`server running at ${SERVER_PORT}`)
