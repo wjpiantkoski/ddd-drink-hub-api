5a8bdb454ceaaf216c33b2d07549fb931f8eacb2
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-18 09:13:04 -0300

    chore: add database env values

--- src/env.ts
+++ src/env.ts
@@ -6,3 +6,7 @@ export const PASSWORD_HASH_SALT_ROUNDS = process.env.PASSWORD_HASH_SALT_ROUNDS
 export const JWT_SECRET = process.env.JWT_SECRET
 export const JWT_EXPIRATION_TIME = process.env.JWT_EXPIRATION_TIME
 export const SERVER_PORT = process.env.SERVER_PORT || 3000
+export const DATABASE_NAME = process.env.DATABASE_NAME
+export const DATABASE_USER = process.env.DATABASE_USER
+export const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD
+export const DATABASE_HOST = process.env.DATABASE_HOST
