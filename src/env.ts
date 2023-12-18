92f58875c040ab40d3e13058a9ae3105b08fb245
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-18 08:30:17 -0300

    chore: add SERVER_PORT to env

--- src/env.ts
+++ src/env.ts
@@ -5,3 +5,4 @@ dotenv.config()
 export const PASSWORD_HASH_SALT_ROUNDS = process.env.PASSWORD_HASH_SALT_ROUNDS
 export const JWT_SECRET = process.env.JWT_SECRET
 export const JWT_EXPIRATION_TIME = process.env.JWT_EXPIRATION_TIME
+export const SERVER_PORT = process.env.SERVER_PORT || 3000
