79de590d1a7c02645d079bb389e9defaa9fac435
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-18 12:12:58 -0300

    feat: add DELETE /bookmark/:id endpoint

--- src/infra/http/express/routes/bookmarks.router.ts
+++ src/infra/http/express/routes/bookmarks.router.ts
@@ -1,5 +1,6 @@
 import {Request, Response, Router} from 'express'
 import CreateBookmarkUsecaseFactory from '../../../../modules/beverage/usecases/create-bookmark/create-bookmark.usecase.factory'
+import RemoveBookmarkUsecaseFactory from '../../../../modules/beverage/usecases/remove-bookmark/remove-bookmark.usecase.factory'
 
 export default class BookmarksRouter {
   private _router = Router()
@@ -17,6 +18,21 @@ export default class BookmarksRouter {
       try {
         const createBookmark = CreateBookmarkUsecaseFactory.create()
         const {status, data} = await createBookmark.execute(req.body)
+        res.status(status).send(data)
+      } catch (err) {
+        console.error(err)
+        res.status(500).send()
+      }
+    })
+
+    this.router.delete('/:id', async (req: Request, res: Response) => {
+      try {
+        const removeBookmark = RemoveBookmarkUsecaseFactory.create()
+
+        const {status, data} = await removeBookmark.execute({
+          bookmarkId: req.params.id
+        })
+
         res.status(status).send(data)
       } catch (err) {
         console.error(err)
