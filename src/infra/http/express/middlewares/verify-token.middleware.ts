cdd5f602b069c13081a6e1868baad59784511d67
Willian Piantkoski <willianpiantkoski@Willians-MacBook-Pro.local>
2023-12-18 16:13:25 -0300

    refactor: update how token is verified

--- src/infra/http/express/middlewares/verify-token.middleware.ts
+++ src/infra/http/express/middlewares/verify-token.middleware.ts
@@ -5,12 +5,15 @@ import {JWT_SECRET} from '../../../../env'
 export const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
   const token = req.header('Authorization')
 
+  console.log({token})
+
   if (!token) {
     return res.sendStatus(401)
   }
 
   try {
-    await jwt.verify(token, JWT_SECRET)
+    const tokenValue = token.split(/\s+/)[1]
+    await jwt.verify(tokenValue, JWT_SECRET)
     return next()
   } catch { 
     return res.sendStatus(401)
