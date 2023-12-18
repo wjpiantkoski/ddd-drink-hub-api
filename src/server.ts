a03ab5d0ab76437dac112a4636ef24a8978eb27e
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-18 09:14:02 -0300

    feat: connect database in server initialization

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import App from "./infra/http/express"
 import {SERVER_PORT} from './env'
+import Database from "./infra/@shared/database/database"
 
 class Server {
   private app
@@ -9,10 +10,16 @@ class Server {
   }
 
   public async run(): Promise<void> {
+    const database = new Database()
+
+    await database.connect()
+    console.log('Database is connected')
+
     const server = this.app.listen(SERVER_PORT)
     console.log(`server running at ${SERVER_PORT}`)
 
     process.on('SIGINT', async () => {
+      await database.disconnect()
       server.close()
     })
   }
