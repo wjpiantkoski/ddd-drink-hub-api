400e46c7060e04270537f2b3b128b9b5dd39d0a7
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-18 11:29:33 -0300

    refactor: update RemoveBookmarkUsecase to use UsecaseResponse

--- src/modules/beverage/usecases/remove-bookmark/remove-bookmark.usecase.ts
+++ src/modules/beverage/usecases/remove-bookmark/remove-bookmark.usecase.ts
@@ -1,3 +1,4 @@
+import UsecaseResponse from "../../../../@shared/domain/usecase/usecase-response";
 import IUsecase from "../../../../@shared/domain/usecase/usecase.interface";
 import IBookmarkRepository from "../../repository/bookmark.repository.interface";
 
@@ -9,7 +10,12 @@ export default class RemoveBookmarkUsecase implements IUsecase {
 
   constructor(private bookmarkRepository: IBookmarkRepository) {}
 
-  async execute(input: RemoveBookmarkUsecaseInput): Promise<void> {
+  async execute(input: RemoveBookmarkUsecaseInput): Promise<UsecaseResponse> {
     await this.bookmarkRepository.deleteById(input.bookmarkId)
+    
+    return {
+      status: 204,
+      data: null
+    }
   }
 }
