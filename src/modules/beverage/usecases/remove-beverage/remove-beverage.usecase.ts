4e0ce4b49aeb4ebb898b0d3f8644d6528a0dd691
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-18 11:22:09 -0300

    refactor: update RemoveBeverageUsecase to use UsecaseResponse

--- src/modules/beverage/usecases/remove-beverage/remove-beverage.usecase.ts
+++ src/modules/beverage/usecases/remove-beverage/remove-beverage.usecase.ts
@@ -1,3 +1,4 @@
+import UsecaseResponse from "../../../../@shared/domain/usecase/usecase-response";
 import IUsecase from "../../../../@shared/domain/usecase/usecase.interface";
 import IBeverageRepository from "../../repository/beverage.repository.interface";
 
@@ -9,7 +10,12 @@ export default class RemoveBeverageUsecase implements IUsecase {
 
   constructor(private beverageRepository: IBeverageRepository) {}
 
-  async execute(input: RemoveBeverageUsecaseInput): Promise<void> {
+  async execute(input: RemoveBeverageUsecaseInput): Promise<UsecaseResponse> {
     await this.beverageRepository.deleteById(input.beverageId)
+    
+    return {
+      status: 204,
+      data: null
+    }
   }
 }
