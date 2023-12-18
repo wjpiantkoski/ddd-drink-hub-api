035c4606c7e78ce3de43dcd305bf089ebbcb2ea6
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-18 10:22:00 -0300

    feat: add POST /users/sign-in endpoint

--- src/infra/http/express/routes/users.router.ts
+++ src/infra/http/express/routes/users.router.ts
@@ -1,5 +1,6 @@
 import {Request, Response, Router} from 'express'
 import UserRegisterUsecaseFactory from '../../../../modules/user/usecases/user-register/user-register.usecase.factory'
+import UserLoginUsecaseFactory from '../../../../modules/user/usecases/user-login/user-login.usecase.factory'
 
 export default class UsersRouter {
   private _router = Router()
@@ -16,6 +17,12 @@ export default class UsersRouter {
     this.router.post('/sign-up', async (req: Request, res: Response) => {
       const userRegisterUsecase = UserRegisterUsecaseFactory.create()
       const {status, data} = await userRegisterUsecase.execute(req.body)
+      res.status(status).send(data)
+    })
+
+    this.router.post('/sign-in', async (req: Request, res: Response) => {
+      const userLoginUsecase = UserLoginUsecaseFactory.create()
+      const {status, data} = await userLoginUsecase.execute(req.body)
       res.status(status).send(data)
     })
   }
