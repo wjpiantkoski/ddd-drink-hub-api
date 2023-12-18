34f63327a5f8b2ebc7f9de3b57f0903533f90ece
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-18 11:32:26 -0300

    feat: add GET /users/:id/bookmarks endpoint

--- src/infra/http/express/routes/users.router.ts
+++ src/infra/http/express/routes/users.router.ts
@@ -1,6 +1,7 @@
 import {Request, Response, Router} from 'express'
 import UserRegisterUsecaseFactory from '../../../../modules/user/usecases/user-register/user-register.usecase.factory'
 import UserLoginUsecaseFactory from '../../../../modules/user/usecases/user-login/user-login.usecase.factory'
+import ListUserBookmarksUsecaseFactory from '../../../../modules/beverage/usecases/list-user-bookmarks/list-user-bookmark.usecase.factory'
 
 export default class UsersRouter {
   private _router = Router()
@@ -15,15 +16,37 @@ export default class UsersRouter {
 
   private routes(): void {
     this.router.post('/sign-up', async (req: Request, res: Response) => {
-      const userRegisterUsecase = UserRegisterUsecaseFactory.create()
-      const {status, data} = await userRegisterUsecase.execute(req.body)
-      res.status(status).send(data)
+      try {
+        const userRegisterUsecase = UserRegisterUsecaseFactory.create()
+        const {status, data} = await userRegisterUsecase.execute(req.body)
+        res.status(status).send(data)
+      } catch {
+        res.status(500).send()
+      }
     })
 
     this.router.post('/sign-in', async (req: Request, res: Response) => {
-      const userLoginUsecase = UserLoginUsecaseFactory.create()
-      const {status, data} = await userLoginUsecase.execute(req.body)
-      res.status(status).send(data)
+      try {
+        const userLoginUsecase = UserLoginUsecaseFactory.create()
+        const {status, data} = await userLoginUsecase.execute(req.body)
+        res.status(status).send(data)
+      } catch {
+        res.status(500).send()
+      }
+    })
+
+    this.router.get('/:id/bookmarks', async (req: Request, res: Response) => {
+      try {
+        const listBookmarksUsecase = ListUserBookmarksUsecaseFactory.create()
+
+        const {status, data} = await listBookmarksUsecase.execute({
+          userId: req.params.id
+        })
+
+        res.status(status).send(data)
+      } catch {
+        res.status(500).send()
+      }
     })
   }
 }
