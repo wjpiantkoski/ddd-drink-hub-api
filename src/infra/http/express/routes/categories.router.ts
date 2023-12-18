2875a0684944db3cb04fab379087a46c93ee08a0
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-18 10:49:50 -0300

    feat: add GET /categories/:id/endpoint

--- src/infra/http/express/routes/categories.router.ts
+++ src/infra/http/express/routes/categories.router.ts
@@ -1,5 +1,6 @@
 import {Request, Response, Router} from 'express'
 import ListCategoriesUsecaseFactory from '../../../../modules/beverage/usecases/list-categories/list-categories.usecase.factory'
+import ListBeverageByCategoryUsecaseFactory from '../../../../modules/beverage/usecases/list-beverages-by-category/list-beverage-by-category.usecase.factory'
 
 export default class CategoriesRouter {
   private _router = Router()
@@ -17,6 +18,16 @@ export default class CategoriesRouter {
       const listCategoriesUsecase = ListCategoriesUsecaseFactory.create()
       const {status, data} = await listCategoriesUsecase.execute()
       res.status(status).send(data)
+    })
+
+    this.router.get('/:id/beverages', async (req: Request, res: Response) => {
+      try {
+        const listBeveragesUsecase = ListBeverageByCategoryUsecaseFactory.create()
+        const {status, data} = await listBeveragesUsecase.execute(req.params.id)
+        res.status(status).send(data)
+      } catch {
+        res.status(500).send()
+      }
     })
   }
 }
